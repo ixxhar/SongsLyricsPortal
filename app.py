f7b39ba96f0ca6cce71f0ea81e923f9718f52020
Izhar Hussain <ixxhar@gmail.com>
2019-04-20 21:14:17 +0500

    Completely changed the UI, fixed bugs and made other changes

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
+from validate_email import validate_email
+from flask_share import Share
+
 
 
 app=Flask(__name__)
+
+#initializing the extenstion
+share = Share(app)
+
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -35,18 +42,15 @@ def search():
 
     cur.close()
 
-    models=['Cosine','WordNet','BM25']
+    models=['All','Cosine','WordNet','BM25']
 
-    return render_template('search.html',querydata=querydata[0:25], models=models)
+    return render_template('search.html',querydata=querydata, models=models)
 
     #return render_template('search.html')
 
 
 @app.route('/searched',methods=['GET','POST'])  #reference to specific music searched page
 def searched():
-    a='nothinh'
-    b='thingno'
-    c=''
 
     if request.method=='POST':
         a=request.form['query']
@@ -62,7 +66,27 @@ def searched():
         result = cur.execute("SELECT * FROM songs WHERE id IN (SELECT songid FROM cosinemodel WHERE queryid = %s)",[a])
         data = cur.fetchall()
 
-        app.logger.info(data)
+        #app.logger.info(data)
+
+        if result > 0:
+            return render_template('searched.html',query=a,model=b,c=c,data=data)
+        else:
+            msg='No Song recommeneded'
+            return render_template('searched.html',msg=msg,query=a,model=b,c=c)
+
+        cur.close()
+
+    elif b== 'WordNet':
+        c=2
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        #getting all songs with respect to the searched query id
+        result = cur.execute("SELECT * FROM songs WHERE id IN (SELECT songid FROM wordnetmodel WHERE queryid = %s)",[a])
+        data = cur.fetchall()
+
+        #app.logger.info(data)
 
         if result > 0:
             return render_template('searched.html',query=a,model=b,c=c,data=data)
@@ -73,11 +97,56 @@ def searched():
         cur.close()
 
 
-
-    elif b== 'WordNet':
-        c='2'
     elif b=='BM25':
-        c='3'
+        c=3
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        #getting all songs with respect to the searched query id
+        result = cur.execute("SELECT * FROM songs WHERE id IN (SELECT songid FROM bm25model WHERE queryid = %s)",[a])
+        data = cur.fetchall()
+
+        #app.logger.info(data)
+
+        if result > 0:
+            return render_template('searched.html',query=a,model=b,c=c,data=data)
+        else:
+            msg='No Song recommeneded'
+            return render_template('searched.html',msg=msg,query=a,model=b,c=c)
+
+        cur.close()
+
+
+    elif b=='All':
+        c=4
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        #getting all songs of cosine to the searched query id
+        cosineresult = cur.execute("SELECT * FROM songs WHERE id IN (SELECT songid FROM cosinemodel WHERE queryid = %s)",[a])
+        cosinedata = cur.fetchall()
+
+        #getting all songs of wordnet to the searched query id
+        wordnetresult = cur.execute("SELECT * FROM songs WHERE id IN (SELECT songid FROM wordnetmodel WHERE queryid = %s)",[a])
+        wordnetdata = cur.fetchall()
+
+        #getting all songs of wordnet to the searched query id
+        bm25result = cur.execute("SELECT * FROM songs WHERE id IN (SELECT songid FROM bm25model WHERE queryid = %s)",[a])
+        bm25data = cur.fetchall()
+
+        app.logger.info(cosinedata)
+
+        if cosineresult > 0 or wordnetresult > 0 or bm25result > 0:
+            return render_template('searched.html',query=a,model=b,c=c,cosinedata=cosinedata,wordnetdata=wordnetdata,bm25data=bm25data,checker=1)
+        else:
+            msg='No Song recommeneded'
+            return render_template('searched.html',msg=msg,query=a,model=b,c=c,checker=1)
+
+        cur.close()
+
+
 
     return render_template('searched.html',query=a,model=b,c=c)
 
@@ -97,29 +166,46 @@ class RegisterForm(Form):
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
+
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        email = request.form['email']
+        cpassword = request.form['cpassword']
 
         # Create cursor
         cur = mysql.connection.cursor()
 
-        # Execute query for inserting users
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s OR email = %s", ([username],[email]))
 
-        # Commit to DB
-        mysql.connection.commit()
+        if result > 0:
+            flash('User already registered', 'danger')
+            return render_template('register.html')
+        else:
+            if password==cpassword:
+                password = sha256_crypt.encrypt(password)
+                # Execute query for inserting users
+                cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", ([name], [email], [username], [password]))
+                # Commit to DB
+                mysql.connection.commit()
 
-        # Close connection
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now registered and logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Password Does Not Match', 'danger')
+                return render_template('register.html')
+
         cur.close()
 
-        flash('You are now registered and can log in', 'success')
-
         return redirect(url_for('index'))
-    return render_template('register.html', form=form)
+    return render_template('register.html')
 
 # User login
 @app.route('/login', methods=['GET', 'POST'])
@@ -276,6 +362,27 @@ def artist_song_selected(artist):
     cur.close()
 
     #return redirect(url_for('allsong'),data=data)
+
+#   Selecting single song!
+@app.route('/singlesong/<string:id>', methods=['POST'])
+def singlesong(id):
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get songs by specific artist
+    result = cur.execute("SELECT * from songs where id=%s",[id])
+    data = cur.fetchall()
+
+    link=data[0]['song'].replace(" ", "+")
+
+    if result > 0:
+        return render_template('singlesong.html',data=data,link=link)
+    else:
+        msg='No Song'
+        return render_template('singlesong.html',msg=msg,namee=artist)
+
+    cur.close()
+
 
 if __name__=="__main__":    #main function for the python
     app.secret_key='secretkey1122'
